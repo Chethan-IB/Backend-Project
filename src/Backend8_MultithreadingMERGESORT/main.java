593d4ed5075a5eabf52c82f017ca56ebba0ebb02
Chethan <ibchethan7@gmail.com>
2026-03-02 11:09:25 +0530

    Init Commit

---
+++ src/Backend8_MultithreadingMERGESORT/main.java
@@ -0,0 +1,12 @@
+package Backend8_MultithreadingMERGESORT;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class main {
+    public static void main(String[] args) {
+        List<Integer> numbers= Arrays.asList(4,1,7,2,3,6,8,5,10);
+
+
+    }
+}
